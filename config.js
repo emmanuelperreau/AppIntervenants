599d0f0a48907b0ee1e32388917b822e171159f5
emmanuelperreau <manu.perreau@gmail.com>
2025-12-03 16:44:15 +0100

    feat: Allow multiple agency configurations
    The application now supports multiple agency configurations. The agency is detected via URL parameters. If no parameter is specified, it defaults to "nord-touraine". This change updates the configuration structure and the logic for applying it in the application.

--- config.js
+++ config.js
@@ -1,44 +1,76 @@
-const AGENCY_CONFIG = {
-    // Le nom de l'agence
-    name: "O2 Nord Touraine",
-
-    // Dans l'onglet accueil les deux liens gestion de clés et km
-    home: {
-        gestionClesUrl: "#", 
-        declarationKmsUrl: "#"
-    },
-
-    // Rémunération : le nom de la mutuelle et le prix
-    remuneration: {
-        mutuelleName: "Mutuelle Santé (CPMS)",
-        mutuellePrice: "13,55 €"
-    },
-
-    // Docs : tous les liens, l'adresse et le numéro de la médecine du travail
-    docs: {
-        medecineTravail: {
-            address: "25 Rue de la Milletière, 37100 Tours",
-            phoneDisplay: "02 47 62 88 88",
-            phoneLink: "tel:0247628888"
+const AGENCY_CONFIGS = {
+    // CONFIGURATION NORD TOURAINE (Par défaut)
+    "nord-touraine": {
+        name: "O2 Nord Touraine",
+        home: {
+            gestionClesUrl: "https://docs.google.com/forms/d/e/1FAIpQLScYpiJS_KAWRl3h_dJCY2-cX9gYJr-Uh9qNQK2qVZ8xFNY14w/viewform?usp=sf_link", 
+            declarationKmsUrl: "https://docs.google.com/forms/d/e/1FAIpQLScIt7TJMrf9qr-h3xf9Ryut_PdVmSm1LRxp6_-xh4tqIqR1Tg/viewform?usp=sf_link"
         },
-        links: {
-            avantages: "#", // Avantages et Rémunération
-            conges: "#", // Pose des congés payés
-            dueSante: "#", // DUE Santé
-            garantieSante: "#", // Garantie Frais de Santé
-            dueInteressement: "#", // DUE Intéressement
-            accordTemps: "https://drive.google.com/file/d/1Akowb6VYpWDSeCs28psDmaRdqjhWnHYl/view?usp=sharing" // Accord d'aménagement
+        remuneration: {
+            mutuelleName: "Mutuelle Santé (CPMS)",
+            mutuellePrice: "16,88 €"
+        },
+        docs: {
+            medecineTravail: {
+                address: "25 Rue de la Milletière, 37100 Tours",
+                phoneDisplay: "02 47 62 88 88",
+                phoneLink: "tel:0247628888"
+            },
+            links: {
+                avantages: "#",
+                conges: "#",
+                dueSante: "#",
+                garantieSante: "#",
+                dueInteressement: "#",
+                accordTemps: "https://drive.google.com/file/d/1Akowb6VYpWDSeCs28psDmaRdqjhWnHYl/view?usp=sharing"
+            }
+        },
+        contacts: {
+            agence: {
+                phoneDisplay: "02 46 65 68 80",
+                phoneLink: "tel:0246656880",
+                address: "134 Bd Charles de Gaulle, 37540 Saint-Cyr-Sur-Loire",
+                addressLink: "https://maps.google.com/?q=134+Bd+Charles+de+Gaulle+37540+Saint-Cyr-Sur-Loire",
+                email: "nordtouraine@o2.fr"
+            }
         }
     },
 
-    // Contacts et urgences : juste le tel, l'adresse et le mail de l'agence O2
-    contacts: {
-        agence: {
-            phoneDisplay: "02 46 65 68 80",
-            phoneLink: "tel:0246656880",
-            address: "134 Bd Charles de Gaulle, 37540 Saint-Cyr-Sur-Loire",
-            addressLink: "https://maps.google.com/?q=134+Bd+Charles+de+Gaulle+37540+Saint-Cyr-Sur-Loire",
-            email: "nordtouraine@o2.fr"
+    // CONFIGURATION LOCHES (À compléter)
+    // Url à utiliser : /?agence=loches
+    "loches": {
+        name: "O2 Loches",
+        home: {
+            gestionClesUrl: "https://docs.google.com/forms/d/e/1FAIpQLSczzwX9PjMXBJHHAq6TnlUQSfCSk2yLkoP1BtbVyePPAAluGQ/viewform?usp=sf_link", 
+            declarationKmsUrl: "https://docs.google.com/forms/d/e/1FAIpQLSeLIa2CutbQn7QVCdDbJJfr8KjCeHAj7PpAG1qhTYl1bkF9Lw/viewform?usp=sf_link"
+        },
+        remuneration: {
+            mutuelleName: "Mutuelle Santé (ALLIANZ)",
+            mutuellePrice: "20,41 €"
+        },
+        docs: {
+            medecineTravail: {
+                address: "1 Pl. du Maréchal Leclerc, 37600 Beaulieu-lès-Loches",
+                phoneDisplay: "02 47 37 66 76",
+                phoneLink: "tel:0247376676"
+            },
+            links: {
+                avantages: "#",
+                conges: "#",
+                dueSante: "#",
+                garantieSante: "#",
+                dueInteressement: "#",
+                accordTemps: "#"
+            }
+        },
+        contacts: {
+            agence: {
+                phoneDisplay: "02 42 22 00 57",
+                phoneLink: "tel:0242220057",
+                address: "31 Grande Rue, 37600 Loches",
+                addressLink: "https://maps.google.com/?q=31+Grande+Rue+37600+Loches",
+                email: "loches@o2.fr"
+            }
         }
     }
 };
